b4f932ed0363e40e2762a8695a90ce24bfc22819
ADS\t_bhoop <pankaj.bhootra@autodesk.com>
2017-09-28 10:50:57 +0800

    Don't allow user to access login/register/forgotPassword pages if already logged in

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 // Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
+router.get('/', ensureAuthenticated, function(req, res) {
 	res.render('index');
 });
 
-function ensureAuthenticated(req, res, next){
+function ensureAuthenticated(req, res, next) {
 	if(req.isAuthenticated()){
 		return next();
 	} else {
-		req.flash('error_msg','You are not logged in');
+		req.flash('error_msg','You are not logged in.');
 		res.redirect('/users/login');
+	}
+}
+
+// Don't allow user to access login/register page if already logged in
+router.get('/users/login', ensureNotAuthenticated, function(req, res) {
+	res.render('login');
+});
+
+router.get('/users/register', ensureNotAuthenticated, function(req, res) {
+	res.render('register');
+});
+
+router.get('/users/forgotPassword', ensureNotAuthenticated, function(req, res) {
+	res.render('forgotPassword');
+});
+
+function ensureNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		req.flash('error_msg', 'You are already logged in.');
+		res.redirect('/');
+	}	else {
+		return next();
 	}
 }
 
